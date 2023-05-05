49b62d551cb63ffc56050a1710c6b997efa45f12
DelroyGayle <delroygayle01@gmail.com>
2023-05-05 01:20:24 +0000

    Experiment with the size of 'rules-container' 150% for mobiles

--- assets/js/rpsln.js
+++ assets/js/rpsln.js
@@ -1,6 +1,7 @@
 /* jshint esversion: 11 */
 // Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function () {
+   console.log(window.innerWidth,window.innerHeight)
    runTheGame();
 });
 
